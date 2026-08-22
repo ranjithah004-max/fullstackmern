d2fd9fc9338c5127fcad882114df1fd77636aab8
ranjithah004-max <ranjithah004@gmail.com>
2026-08-22 12:33:58 +0530

    Fix counter hook integration

--- frontend/src/pages/Service.jsx
+++ frontend/src/pages/Service.jsx
@@ -1,8 +1,16 @@
+import useCounter from "./useCounter"
+
 const Service =() =>{
+    const{count,increment,decrement,reset}=useCounter()
     return (
         <div>
             <h2>About us</h2>
+            <h3>customhook</h3>
+            <h4>Count:{count}</h4>
             <p>you want any services</p>
+            <button onClick={increment}>+</button>
+            <button onClick={decrement}>-`</button>
+            <button onClick={reset}>reset</button>
         </div>
     )
 }
